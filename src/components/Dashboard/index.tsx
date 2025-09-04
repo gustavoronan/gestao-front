bcc8fe91d056f668caa4971d9230afa1fd407020
Gustavo Ronan <gustavoronn@gmail.com>
2025-09-04 11:04:03 -0300

    layout base

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -2,49 +2,58 @@ import { PieChart } from "@mui/x-charts";
 import { BarChart } from "@mui/x-charts/BarChart";
 import "./style.css";
 export default function Dashboard() {
+
+  const despesas = [
+    { descricao: "Compras no supermercado", valor: 17, categoria: "Alimentação" },
+    { descricao: "Troca de óleo do carro", valor: 15, categoria: "Transporte" },
+    { descricao: "Reserva de emergência", valor: 20, categoria: "Emergência" },
+    { descricao: "Assinatura da Netflix", valor: 20, categoria: "Lazer" },
+    { descricao: "Jantar", valor: 20, categoria: "Alimentação" },
+  ];
+
+const categoriasAgrupadas = despesas.reduce<Record<string, number>>((acc, despesa) => {
+  acc[despesa.categoria] = (acc[despesa.categoria] || 0) + despesa.valor;
+  return acc;
+}, {});
+
+  const categorias = Object.keys(categoriasAgrupadas);
+  const valores = Object.values(categoriasAgrupadas);
+
   return (
-    <>
-    <div className="container">
+    <div className="dashboard">
+      <div className="bar">
+        <BarChart
+          xAxis={[
+            {
+              id: "barCategories",
+              data: categorias,
+            },
+          ]}
+          series={[
+            {
+              data: valores,
+            },
+          ]}
+          height={300}
+        />
+        <h3>Categorias</h3>
+      </div>
 
-
-      <div className="dashboard">
-        <div className="bar">
-          <h3>Categorias</h3>
-          <BarChart
-            xAxis={[
-              {
-                id: "barCategories",
-                data: ["bar A", "bar B", "bar C"],
-              },
-            ]}
-            series={[
-              {
-                data: [2, 5, 3],
-              },
-            ]}
-            height={300}
-            width={500}
-          />
-        </div>
-
-        <div className="pie">
+      <div className="pie">
+        <PieChart
+          series={[
+            {
+              data: despesas.map((d, index) => ({
+                id: index,
+                value: d.valor,
+                label: d.descricao,
+              })),
+            },
+          ]}
+          height={300}
+        />
         <h3>Despesas</h3>
-          <PieChart
-            series={[
-              {
-                data: [
-                  { id: 0, value: 10, label: "series A" },
-                  { id: 1, value: 15, label: "series B" },
-                  { id: 2, value: 20, label: "series C" },
-                ],
-              },
-            ]}
-            height={300}
-             width={300}
-          />
-        </div>
       </div>
     </div>
-    </>
   );
 }
